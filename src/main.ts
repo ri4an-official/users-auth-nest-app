fc3c666219d9d0a89aa72984d5bf46b6a12c69b6
ri4an <vadimguber7@gmail.com>
2022-08-31 18:39:47 +0600

    add feature cats

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { AppModule } from './app/app.module'
 
+const PORT = 3000
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	await app
+		.listen(PORT)
+		.then(() => console.log(`\nStarted on http://localhost:${PORT}`))
 }
 bootstrap()
