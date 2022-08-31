7fe164c111f4214ac6c72000c8b1777ed8d68f02
ri4an <vadimguber7@gmail.com>
2022-08-31 20:47:46 +0600

    init app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
 
-const PORT = 3000
-async function bootstrap() {
+async function start() {
+	const PORT = process.env.PORT ?? 3000
 	const app = await NestFactory.create(AppModule)
+
 	await app
 		.listen(PORT)
 		.then(() => console.log(`\nStarted on http://localhost:${PORT}`))
 }
-bootstrap()
+start()
